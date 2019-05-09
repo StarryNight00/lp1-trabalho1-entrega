431f32f5a33d57f0f723f5a35aad79dd19f69287
StarryNight00 <cisa.matias@gmail.com>
2019-05-09 11:27:35 +0100

    Add project files.

---
+++ trab1_LP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trab1_LP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
